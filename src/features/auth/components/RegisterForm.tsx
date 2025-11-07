10f00dd6485249b2107d36c44b1cd8ad7b05d40c
e-mandy <andymfrd02@gmail.com>
2025-11-07 20:40:25 +0100

    Mise en place de l'UI du register - Terminé

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -4,7 +4,6 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import type { RegisterUser } from "../schemas";
 import { registerSchema } from "../schemas";
 import { Eye, Mail } from 'lucide-react';
-import bg_auth from '../../../assets/auth/bg_auth.png'
 
 function RegisterForm(){
 
@@ -27,73 +26,71 @@ function RegisterForm(){
     return (
         <div className="flex h-screen w-screen">
             <div className="w-1/2">
-                <h3>LOGO</h3>
-                <div>
-                    <h1>C'est Parti !</h1>
-                    <p>Bienvenue sur Wá xo, fait par des étudiants pour les étudiants.</p>
+                <h3 className="text-3xl font-bold p-2">LOGO</h3>
+                <div className="flex flex-col items-center mb-10">
+                    <h1 className="mt-10 text-5xl font-extrabold mb-3">C'est Parti !</h1>
+                    <p className="w-2/3 text-center">Bienvenue sur Wá xo, fait par des étudiants pour les étudiants.</p>
                 </div>
-                <div>
-                    <div>
-                        <p>S'inscrire avec Google</p>
-                    </div>
-                    <div>
+                <div className="flex flex-col w-3/5 items-center mx-auto gap-10">
+                    <button className="w-full google-button bg-[#F5F5F5] font-medium cursor-pointer">S'inscrire avec Google</button>
+                    <div className="w-full">
                         <form onSubmit={handleSubmit(onSubmit)}>
-                            <div>
+                            <div className="flex gap-4 mb-6">
                                 <div>
                                     <label htmlFor="lastname">Nom</label>
-                                    <div>
-                                        <input type="text" {...register("lastname")} id="lastname"/>
+                                    <div className="auth-input-style">
+                                        <input type="text" className="input-disabled" {...register("lastname")} id="lastname"/>
                                         {/* L'icon */}
                                     </div>
-                                    { errors.lastname?.message ?? (<span>{errors.lastname?.message}</span>)}
+                                    { errors.lastname?.message ?? (<span className="error-message">{errors.lastname?.message}</span>)}
                                 </div>
                                 <div>
                                     <label htmlFor="firstname">Prénom</label>
-                                    <div>
-                                        <input type="text" {...register("firstname")} id="firstname"/>
+                                    <div className="auth-input-style">
+                                        <input type="text" className="input-disabled" {...register("firstname")} id="firstname"/>
                                         {/* L'icon */}
                                     </div>
-                                    { errors.firstname?.message ?? (<span>{errors.firstname?.message}</span>)}
+                                    { errors.firstname?.message ?? (<span className="error-message">{errors.firstname?.message}</span>)}
                                 </div>
                             </div>
-                            <div>
+                            <div className="mb-6">
                                 <label htmlFor="email">Adresse mail</label>
-                                <div>
-                                    <input type="email" {...register("email")} id="email"/>
+                                <div className="auth-input-style">
+                                    <input type="email" className="input-disabled" {...register("email")} id="email"/>
                                     <Mail />
                                 </div>
-                                { errors.email?.message ?? (<span>{errors.email?.message}</span>)}
+                                { errors.email?.message ?? (<span className="error-message">{errors.email?.message}</span>)}
                             </div>
                             
-                            <div>
+                            <div className="mb-6">
                                 <label htmlFor="password">Mot de passe</label>
-                                <div>
-                                    <input type="password" {...register("password")} id="password"/>
+                                <div className="auth-input-style">
+                                    <input type="password" className="input-disabled" {...register("password")} id="password"/>
                                     <Eye />
                                 </div>
-                                { errors.password?.message ?? (<span>{errors.password?.message}</span>)}
+                                { errors.password?.message ?? (<span className="error-message">{errors.password?.message}</span>)}
                             </div>
-                            <div>
+                            <div className="">
                                 <label htmlFor="password_confirmation">Confirmer le mot de passe</label>
-                                <div>
-                                    <input type="password" {...register("password_confirmation")} id="password_confirmation"/>
+                                <div className="auth-input-style">
+                                    <input type="password" className="input-disabled" {...register("password_confirmation")} id="password_confirmation"/>
                                     <Eye />
                                 </div>
-                                    { errors.password_confirmation?.message ?? (<span>{errors.password_confirmation?.message}</span>)}
+                                    { errors.password_confirmation?.message ?? (<span className="error-message">{errors.password_confirmation?.message}</span>)}
                             </div>
 
 
-                            <button type="submit" disabled={isSubmitting}>
+                            <button className="global-button w-full mt-10 bg-[#4399CB] text-white font-bold text-xl cursor-pointer" type="submit" disabled={isSubmitting}>
                                 { isSubmitting ? "Loading..." : "S'inscrire" }
                             </button>
                         </form>
                     </div>
                 </div>
             </div>
-            <div className="bg-auth bg-red-300 w-1/2 relative">
+            <div className="bg-auth w-1/2 relative">
                 <div className="absolute right-7 top-5 button bg-[#4399CB] text-white">Connexion</div>
                 <div className="absolute p-4 bottom-10 w-full">
-                    <h4 className="text-3xl font-bold text-white w-2/3 mb-3">Le succès est de passer d’un échec à l’autre sans perdre son enthousiasme.</h4>
+                    <h4 className="text-3xl font-bold text-white w-4/5 mb-3 text-start">Le succès est de passer d’un échec à l’autre sans perdre son enthousiasme.</h4>
                     <span className="block text-white">Winstone Churchill</span>
                 </div>
             </div>
