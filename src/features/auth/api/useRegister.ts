e3a4992264abaccd2bcb5901cd1d745a679dc986
e-mandy <andymfrd02@gmail.com>
2025-11-10 20:22:29 +0100

    Mise en place de la communication entre Zustand et RHF - Terminé

--- src/features/auth/api/useRegister.ts
+++ src/features/auth/api/useRegister.ts
@@ -1,12 +1,17 @@
 import { useMutation } from '@tanstack/react-query'
 import type { RegisterUser } from '../schemas'
 
+type ApiResponse = {
+    user: RegisterUser,
+    token: string
+}
 
-const sendUser = async (user: RegisterUser) => {
+
+const sendUser = async (user: RegisterUser): Promise<ApiResponse> => {
     return await new Promise((resolve) => {
         setTimeout(() => {
-            const result = { token: "Résultat", user: user }
-            resolve(result.user)
+            const result = { user: user, token: "Resultat" }
+            resolve(result)
         }, 2000)
     })
 }
